b50e172cf5d7e53fc472193a8ae3f7408a8e9903
unknown <julia.constant@live.nl>
2022-07-11 21:01:52 +0200

    add hook

--- test_flask_app.py
+++ test_flask_app.py
@@ -1,10 +1,12 @@
 import random
 
 def number():
-    return random.randint(0, 10)
+    return random.randint(11, 15)
 
 def test_number():
     no = number()
     assert 0 <= no <= 10 
 
 # https://realpython.com/pytest-python-testing/ 
+# https://towardsdatascience.com/github-automated-testing-python-fdfe5aec9446
+
